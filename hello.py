c83774a1ee5917b6e0b517b3df0e8aa3e66e0f65
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-05-26 18:02:43 -0300

    course MD

---
+++ hello.py
@@ -0,0 +1,33 @@
+#Hola mundo
+#Otro comment
+print("hello world")
+print('Hello World')
+
+"""
+Este es un comment 
+de varias 
+lineas
+"""
+
+'''
+Este es un comment 
+de varias 
+lineas
+'''
+
+#Consulta el tipo de dato
+print(type("Soy un dato str")) #tipo str
+print(type(12)) #tipo int
+print(type(12.5)) #tipo float
+print(type(True)) #tipo bool
+print(type(False)) #tipo bool
+print(type(None)) #tipo None
+print(type([])) #tipo list
+print(type(())) #tipo tuple
+print(type({})) # tipo dict
+print(type(set())) # tipo set
+print(type(print)) # tipo function
+print(isinstance(12, int))
+
+
+
